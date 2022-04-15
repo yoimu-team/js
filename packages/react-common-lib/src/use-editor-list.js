5c6dd422b508f775b9cef10d0a9e10070e7b820c
吳俊緯 <wcw850710@gmail.com>
2022-04-15 13:34:53 +0800

    refactor(@yoimu/react-common-lib@0.2.2): 修正 use-editor-list 會修改到原數據的陋習

--- packages/react-common-lib/src/use-editor-list.js
+++ packages/react-common-lib/src/use-editor-list.js
@@ -1,58 +1,55 @@
-import { useCallback } from 'react'
 import { useSafeState } from './use-safe-state'
+import produce from 'immer'
 
 export const useEditorList = (initialValueFunc, init = true) => {
 	const [list, setList] = useSafeState(() => (init ? [initialValueFunc()] : []))
 
-	const create = useCallback(
-		() => setList(e => [...e, initialValueFunc()]),
-		[setList],
+	const create = setList(
+		produce(d => {
+			d.push(initialValueFunc())
+		}),
 	)
 
-	const editByIndex = useCallback(
-		(index, value) =>
-			setList(e => {
-				if (index < 0 || index > e.length - 1) return e
-				const e2 = e.slice()
-				e2[index] = value
-				return e2
-			}),
-		[setList],
-	)
+	const editByIndex = (index, value) => {
+		if (index < 0 || index > list.length - 1) return
 
-	const editKeyValueByIndex = useCallback(
-		(index, key, value) =>
-			setList(e => {
-				if (index < 0 || index > e.length - 1) return e
-				const e2 = e.slice()
-				e2[index] = { ...e2[index], [key]: value }
-				return e2
+		setList(
+			produce(d => {
+				d[index] = value
 			}),
-		[setList],
-	)
+		)
+	}
 
-	const removeByIndex = useCallback(
-		(index, removeNum = 1) =>
-			setList(e => {
-				if (index < 0 || index > e.length - 1) return e
-				const e2 = e.slice()
-				e2.splice(index, removeNum)
-				return e2
-			}),
-		[setList],
-	)
+	const editKeyValueByIndex = (index, key, value) => {
+		if (index < 0 || index > list.length - 1) return
 
-	const removeByKeyValue = useCallback(
-		(key, value, removeNum = 1) =>
-			setList(e => {
-				const removeIndex = e.findIndex(f => f[key] === value)
-				if (removeIndex === -1) return e
-				const e2 = e.slice()
-				e2.splice(removeIndex, removeNum)
-				return e2
+		setList(
+			produce(d => {
+				d[index][key] = value
 			}),
-		[setList],
-	)
+		)
+	}
+
+	const removeByIndex = (index, removeNum = 1) => {
+		if (index < 0 || index > list.length - 1) return
+
+		setList(
+			produce(d => {
+				d.splice(index, removeNum)
+			}),
+		)
+	}
+
+	const removeByKeyValue = (key, value, removeNum = 1) => {
+		const removeIndex = list.findIndex(e => e[key] === value)
+		if (removeIndex === -1) return
+
+		setList(
+			produce(d => {
+				d.splice(removeIndex, removeNum)
+			}),
+		)
+	}
 
 	return [
 		list,
