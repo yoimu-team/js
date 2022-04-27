74fa3d09f64ea849f04e383947725f5e0da3b9a2
吳俊緯 <wcw850710@gmail.com>
2022-04-27 17:47:14 +0800

    fix(@yoimu/common-lib@0.0.5): stepPrice 代碼優化且小數點後數字不加逗號

--- packages/common-lib/src/step-price.js
+++ packages/common-lib/src/step-price.js
@@ -1,21 +1,29 @@
 export const stepPrice = (() => {
-	const ds = {}
-	const createStepD = step =>
-		(ds[step] = Array.from(new Array(step), (_, i) => i).reduce(
+	const stepRegs = {}
+	const createStepReg = step => {
+		const sp = Array.from(new Array(step), (_, i) => i).reduce(
 			p => p + '\\d',
 			'',
-		))
+		)
+		stepRegs[step] = new RegExp(`(\\d)(?=(${sp})+(?!\\d))`, 'g')
+		return stepRegs[step]
+	}
 	return (price, step = 3) => {
 		let _price = price
 
 		if (typeof price === 'number') _price = String(price)
 
 		if (typeof _price === 'string') {
-			const stepD = ds[step] || createStepD(step)
-			_price = _price.replace(
-				new RegExp(`(\\d)(?=(${stepD})+(?!\\d))`, 'g'),
-				'$1,',
-			)
+			const priceReg = stepRegs[step] || createStepReg(step)
+			let sptPrices = []
+			if (_price.includes('.')) {
+				sptPrices = _price.split('.')
+			}
+			if (sptPrices.length) {
+				_price = sptPrices[0].replace(priceReg, '$1,') + '.' + sptPrices[1]
+			} else {
+				_price = _price.replace(priceReg, '$1,')
+			}
 		}
 		return _price
 	}
