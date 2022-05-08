b16d5af8b8f135e77d0774e8b6af273f39a94f6e
吳俊緯 <wcw850710@gmail.com>
2022-05-08 22:29:49 +0800

    refactor(common-lib/declare-enum): 優化語法

--- packages/common-lib/src/declare-enum.js
+++ packages/common-lib/src/declare-enum.js
@@ -1,54 +1,53 @@
 /*
   使用範例
   申明：
-  值 or [值(key), 翻譯(t), 描述(d)]
+  值(v) or [值(v), 翻譯(t), 描述(d)]
   declareEnum({ START: [0], END: [1] })
   declareEnum({ START: [0, '開始'], END: [1, '結束'] })
   declareEnum({ START: [0, '開始', 'START'], END: [1, '結束', 'END'] })
  */
 export const declareEnum = (obj, typeBindValue) => {
-	const v = {}
-	const t = {}
-	const d = {}
 	const keys = []
 	let length = 0
 	const map = callback => {
 		if (!callback) return []
 
-		const result = []
+		const list = []
 		for (let i = 0; i < keys.length; i++) {
 			const k = keys[i]
-			result.push(callback(v[k], k, i))
+			list.push(callback(result[k], k, i))
 		}
-		return result
+		return list
 	}
+	const result = { keys, length, map }
 
 	if (typeof obj === 'object') {
 		for (const k in obj) {
 			const e = obj[k]
 
-			if (Array.isArray(e)) {
-				const first = e[0]
-				v[k] = first
-				for (let i = 1; i < e.length; i++) {
-					const f = e[i]
-					switch (i) {
-						case 1:
-							t[first] = f
-							break
-						case 2:
-							d[first] = f
-							break
-					}
-				}
-			} else {
-				v[k] = e
-			}
-
 			keys.push(k)
 			length += 1
+
+			if (Array.isArray(e)) {
+				const v = e[0]
+				result[k] = v
+
+				for (let i = 1; i < e.length; i++)
+					switch (i) {
+						case 1:
+							if (result.t == null) result.t = {}
+							result.t[v] = e[i]
+							break
+						case 2:
+							if (result.d == null) result.d = {}
+							result.d[v] = e[i]
+							break
+					}
+			} else {
+				result[k] = e
+			}
 		}
 	}
 
-	return { ...v, t, d, keys, length, map }
+	return result
 }
