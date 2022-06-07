01ef41ed89e286c6a236fdd1553562e9c4735839
吳俊緯 <wcw850710@gmail.com>
2022-06-07 15:06:12 +0800

    feat(react-vite-spa): 優化

--- packages/playground/react-vite-spa/src/core/routes.jsx
+++ packages/playground/react-vite-spa/src/core/routes.jsx
@@ -9,52 +9,38 @@ import { CardWrap } from '@/core/components/card-wrap'
 export const Routes = () => {
 	return (
 		// <PathnameHistoriesProvider>
-			<Switch>
-				<RouteWrapper
-					path={'/login'}
-					exact
-					component={withSuspenseRoute(
-						lazy(() => import('@/pages/login')),
-						LayoutFallback,
-						withTitle('登入'),
-					)}
-					layout={CardWrap}
-				/>
+		<Switch>
+			<RouteWrapper
+				path={'/login'}
+				exact
+				component={withSuspenseRoute(
+					lazy(() => import('@/pages/login')),
+					LayoutFallback,
+					withTitle('登入'),
+				)}
+				layout={CardWrap}
+			/>
 
-				<RouteWrapper
-					path={'/register'}
-					exact
-					component={withSuspenseRoute(
-						lazy(() => import('@/pages/register')),
-						LayoutFallback,
-						withTitle('註冊'),
-					)}
-					layout={CardWrap}
-				/>
+			<RouteWrapper
+				path={'/register'}
+				exact
+				component={withSuspenseRoute(
+					lazy(() => import('@/pages/register')),
+					LayoutFallback,
+					withTitle('註冊'),
+				)}
+				layout={CardWrap}
+			/>
 
-				{/* LayoutRoute START */}
-				{/*<RouteWrapper*/}
-				{/*	path={'/billboard'}*/}
-				{/*	exact*/}
-				{/*	component={withSuspensePrivateRoute(*/}
-				{/*		lazy(() => import('@/pages/billboard')),*/}
-				{/*		LayoutFallback,*/}
-				{/*		withTitle('公佈欄'),*/}
-				{/*		withSideKey('billboard'),*/}
-				{/*	)}*/}
-				{/*	layout={Layout}*/}
-				{/*/>*/}
-				{/* LayoutRoute END */}
-
-				<RouteWrapper
-					component={withSuspenseRoute(
-						lazy(() => import('@/core/components/not-found')),
-						LayoutFallback,
-						withTitle('找不到頁面'),
-					)}
-					layout={CardWrap}
-				/>
-			</Switch>
+			<RouteWrapper
+				component={withSuspenseRoute(
+					lazy(() => import('@/core/components/not-found')),
+					LayoutFallback,
+					withTitle('找不到頁面'),
+				)}
+				layout={CardWrap}
+			/>
+		</Switch>
 		// </PathnameHistoriesProvider>
 	)
 }
